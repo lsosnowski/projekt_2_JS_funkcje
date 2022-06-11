eddc4ee8e3cf1be26fcb4752293f0d2180732086
l.sosnowski <albercik33@icloud.com>
2022-06-11 11:52:54 +0200

    add1

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // Funkcje i zdarzenia
 // typy prtoste: string, number, undefined
 
-function showUser(username,age) {
 
-    console.log(`To jest ${username} i ma ${age} lat`);
+// function showUser(username,age) {
 
+//     console.log(`To jest ${username} i ma ${age} lat`);
+
+// }
+
+// showUser("Dawid", 12);
+
+
+// NApisz trzyu funkcje
+// dodawanie do siebie 3 liczb
+// oblixczanie pola trójkątq
+//dodaj dwie liczny i podziel przez 3
+
+
+const addNums = (a,b,c) => {
+       
+    console.log(a + b + c);
 }
 
-showUser("Dawid", 12);
+addNums(1, 2, 3);
 
 
 
+const poleTr = (a,h) => {
+       
+    console.log(0.5*a*h);
+}
 
+poleTr(1,2);
