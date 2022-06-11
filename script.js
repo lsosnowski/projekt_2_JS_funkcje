f6234fdd74ae3ef380d9932d1da022364c6cc93a
l.sosnowski <albercik33@icloud.com>
2022-06-11 11:35:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Funkcje i zdarzenia
+// typy prtoste: string, number, undefined
+
+function showUser(username,age) {
+
+    console.log(`To jest ${username} i ma ${age} lat`);
+
+}
+
+showUser("Dawid", 12);
+
+
+
+
