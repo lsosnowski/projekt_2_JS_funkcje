4014b91ba640b1e2f34d95690b9b7a5e61fcf9ac
l.sosnowski <albercik33@icloud.com>
2022-06-11 13:43:44 +0200

    add2

--- script.js
+++ script.js
@@ -17,18 +17,85 @@
 //dodaj dwie liczny i podziel przez 3
 
 
-const addNums = (a,b,c) => {
+// const addNums = (a,b,c) => {
        
-    console.log(a + b + c);
+//     console.log(a + b + c);
+// }
+
+// addNums(1, 2, 3);
+
+
+
+// const poleTr = (a,h) => {
+       
+//     console.log(0.5*a*h);
+// }
+
+// poleTr(1.5,2);
+
+
+
+
+// const scoreText = document.getElementById("score");
+
+// const addNums = (a,b) => {
+
+//     console.log(a+b);
+//     const result = a+b;
+//     // wyciaganie wyniku na stronę
+//     return result; 
+// }
+
+// scoreText.textContent = addNums(2,4);
+
+
+
+
+
+
+//pobieramy z html dane
+
+const title = document.getElementById("Title");
+const btntitle = document.getElementById("SetTitle");
+
+//tworzymy fujnkcje
+
+const changeTitle = () => {
+    title.textContent = "Witamy w Katowicach";
+    return;
+
 }
 
-addNums(1, 2, 3);
 
 
+const changeTitle2 = () => {
+    title.textContent = "Domyślny tytuł";
+    return;
 
-const poleTr = (a,h) => {
-       
-    console.log(0.5*a*h);
 }
 
-poleTr(1,2);
+// wywołanie funkcji
+
+    // changeTitle();
+
+// lub wywołanie kliknieciem
+
+btntitle.addEventListener("click",changeTitle);
+btntitle.addEventListener("dblclick",changeTitle2);
+
+
+//podpinamy input pod js
+
+const userNameInput = document.getElementById("UsernameField");
+
+
+const showInputContent = (event) => {
+    title.textContent = event.target.value;
+   
+}
+
+userNameInput.addEventListener("input", (e) => showInputContent(e));
+
+
+
+
